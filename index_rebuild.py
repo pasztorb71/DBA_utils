c5090deca71edf5cc427e811f63408e9a4b3c154
pasztorb <pasztor.bertalan@gmail.com>
2018-06-14 17:01:14 +0200

    command_list into tuple

--- index_rebuild.py
+++ index_rebuild.py
@@ -10,7 +10,9 @@ from parallel_command_executor_class import parallel_command_executor
 def generate_rebuild_commands(ilist):
   tmplist = []
   for index in ilist:
-    tmplist.append('alter index ' + index[0] + '.' + index[1] + ' rebuild')
+    command = 'alter index ' + index[0] + '.' + index[1] + ' rebuild'
+    taskname = index[0] + '.' + index[1]
+    tmplist.append((command, taskname))
   return tmplist
 
 if __name__ == '__main__':
@@ -26,15 +28,22 @@ if __name__ == '__main__':
 
   """It uses my own method, but it must be replaced by somethin like this:
   con = cx_Oracle.connect(db['user'], db['pass'], dsn_tns, mode=md)"""
+
   con = getDBConnection(host, db)
 
   """This line is for getting unusable indexes on a table"""
   #ilist = get_unusable_indexes(con, schema, table)
 
-  """ilist must look like this
+  """ilist is a list of tuples, must look like this:
   ilist = [(schema_1, indexname_1), ..., (schema_n, indexname_n)]"""
+
   ilist = get_table_indexes(con, schema, table)
+
+  """command_list is a list of tuples, must look like this:
+  command_list = [(command_1, taskname_1), ..., (command_n, taskname_n)]"""
+
   command_list = generate_rebuild_commands(ilist)
+
   if not command_list:
     print('Nothing to do')
     exit(0)
